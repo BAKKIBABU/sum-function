0edf16b2f801963a7792aeceb7984c276f14b05f
BAKKIBABU <108388125+BAKKIBABU@users.noreply.github.com>
2022-08-04 13:04:19 +0530

    Add files via upload

---
+++ fpractise.py
@@ -0,0 +1,42 @@
+# def sum():
+#     a=int(input('Enter the value of a:'))
+#     b=int(input('Enter the value of b:'))
+#     c=a+b
+#     return c
+# print(c)
+# return 'The value of {}+{}is{}'.format(a,b,c)
+# print(sum())
+# def sum(a,b):
+#     c=a+b
+#     print('sum=',c)
+# def subm(a,b):
+#     d=a-b
+#     e=a*b
+# print('sub=','Mul=',e)
+# a=int(input("Enter the value of a:"))
+# b=int(input("Enter the value of b:"))
+# sum(a,b)
+# subm(a,b)
+
+
+# def sum_sub(a,b):
+#     c=h+k
+#     d=h-k 
+#     return c,d
+# h=int(input('Enter the value of a:'))
+# k=int(input('Enter the value of b:'))
+# c,d=sum_sub(h,k)
+# print(c,d)
+
+# def fv(n1,n2):
+#     a=n1+n2
+#     b=n1-n2
+#     c=n1*n2
+#     d=n1/n2
+#     return a,b,c,d
+# x=float(input("Enter the value of x:"))
+# y=float(input("Enter the value of x:"))
+# Ar=fv(x,y)
+# print(ar)
+# print(*Ar)
+#  for i in Ar:print(i)
